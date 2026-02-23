efe8a7d338590bb7b2fe138476a6c391a4e91a5b
Urvi Garg <urvigarg139@gmail.com>
2026-02-23 18:57:57 +0530

    CodeShield UI for BlackBox

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,27 @@
+let chart;
+
+function loadExample(){
+
+code.value=
+`// SQL Injection
+query = "SELECT * FROM users WHERE id = " + user
+db.execute(query)
+
+// XSS
+print("<script>alert('hack')</script>")
+
+// Hardcoded secret
+password="123456"
+
+// Unsafe eval
+eval(query)`
+}
+
 function scan(){
 
-loader.style.display="block";
+document.getElementById("status").innerText="Analyzing...";
+
+setTimeout(()=>{
 
 fetch("/scan",{
 method:"POST",
@@ -10,18 +31,35 @@ body:JSON.stringify({code:code.value})
 .then(r=>r.json())
 .then(d=>{
 
-loader.style.display="none";
-score.innerHTML="Risk Score: "+d.score;
-issues.innerHTML=d.issues.join("<br>");
+status.innerText="Complete";
 
-new Chart(pie,{
+critical.innerText=d.critical;
+high.innerText=d.high;
+medium.innerText=d.medium;
+low.innerText=d.low;
+
+if(chart) chart.destroy();
+
+chart=new Chart(chart,{
 type:"doughnut",
 data:{
 labels:["Critical","High","Medium","Low"],
 datasets:[{
-data:[d.critical,d.high,d.medium,d.low]
+data:[d.critical,d.high,d.medium,d.low],
+backgroundColor:["#ff3b3b","#ff9b21","#ffd93b","#3bff8f"]
 }]
-}
+},
+options:{plugins:{legend:{display:false}}}
 });
+
+let html="";
+d.issues.forEach(i=>{
+html+=`<div class="issue">${i}</div>`;
 });
+
+issues.innerHTML=html;
+
+});
+
+},1200);
 }
