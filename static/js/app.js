86e1d6fabff05451f6f0b7b15af74c6162cc3648
Urvi Garg <urvigarg139@gmail.com>
2026-03-22 11:56:51 +0530

    FIX STATIC PATHS

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,4 @@
 function loadExample() {
-    const textarea = document.getElementById("codeInput");
-
-    textarea.value = "SELECT * FROM users WHERE id = '" + " + user_input + '";
-}
-
-
-async function sendMessage() {
-    const input = document.getElementById("chatInput");
-    const chatBox = document.getElementById("chatBox");
-    const code = document.getElementById("codeDisplay").innerText;
-
-    const message = input.value;
-
-    if (!message) return;
-
-    chatBox.innerHTML += `<div class="user">👤 ${message}</div>`;
-
-    const res = await fetch("/chat", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({ message, code })
-    });
-
-    const data = await res.json();
-
-    chatBox.innerHTML += `<div class="ai">🤖 ${data.reply}</div>`;
-
-    input.value = "";
+    document.getElementById("codeInput").value =
+        "SELECT * FROM users WHERE id = '" + " + user_input + '";
 }
